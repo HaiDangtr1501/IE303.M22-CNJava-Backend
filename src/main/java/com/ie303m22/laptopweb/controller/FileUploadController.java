bfb0b00ea41b2b9b5ac6276293d6d02c0d4548a0
HaiDangtr1501 <haidangtr1501@gmail.com>
2022-05-15 01:07:11 +0700

    upload file

--- src/main/java/com/ie303m22/laptopweb/controller/FileUploadController.java
+++ src/main/java/com/ie303m22/laptopweb/controller/FileUploadController.java
@@ -1,25 +1,76 @@
-// package com.ie303m22.laptopweb.controller;
+package com.ie303m22.laptopweb.controller;
 
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
-// import com.ie303m22.laptopweb.model.ResponseObject;
+import java.util.List;
+import java.util.stream.Collectors;
 
-// @Controller
-// @RequestMapping(path = "/api/v1/FileUpload")
-// public class FileUploadController {
-// 	//this controller receive file/image from client
-// 	@PostMapping("")
-// 	public ResponseEntity<ResponseObject> uploadFile(@RequestParam("file")MultipartFile file){
-// 		try {
-// 			//save files to a folder => use a service 
-// 		}
-// 		catch (Exception exception){
-			
-// 		}
-// 	}
-// }
+import com.ie303m22.laptopweb.model.ResponseObject;
+import com.ie303m22.laptopweb.services.IStorageService;
+
+@Controller
+@RequestMapping(path = "/api/v1/FileUpload")
+public class FileUploadController {
+	//this controller receive file/image from client
+    @Autowired
+    private IStorageService storageService;
+	@PostMapping("")
+	public ResponseEntity<ResponseObject> uploadFile(@RequestParam("file")MultipartFile file){
+		try {
+			//save files to a folder => use a service 
+            String generatedFileName = storageService.storeFile(file);
+            return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "upload file successfully", generatedFileName)
+            );
+		}
+		catch (Exception exception){
+			return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
+                new ResponseObject("failed", exception.getMessage(), "")
+            );
+		}
+	}
+
+	//get img url
+	@GetMapping("/files/{fileName:.+}")
+     // /files/e03171ef00d744348580db9e8b8adb7e.png
+    public ResponseEntity<byte[]> readDetailFile(@PathVariable String fileName) {
+        try {
+            byte[] bytes = storageService.readFileContent(fileName);
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(bytes);
+        }catch (Exception exception) {
+            return ResponseEntity.noContent().build();
+        }
+    }
+	//How to load all uploaded files ?
+    @GetMapping("")
+    public ResponseEntity<ResponseObject> getAllUploadedFiles() {
+        try {
+            List<String> urls = storageService.loadAll()
+                    .map(path -> {
+                        //convert fileName to url(send request "readDetailFile")
+                        String urlPath = MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                                "readDetailFile", path.getFileName().toString()).build().toUri().toString();
+                        return urlPath;
+                    })
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(new ResponseObject("ok", "List files successfully", urls));
+        }catch (Exception exception) {
+            return ResponseEntity.ok(new
+                    ResponseObject("failed", "List files failed", new String[] {}));
+        }
+    }
+}
--- src/main/java/com/ie303m22/laptopweb/services/ImageStorageService.java
+++ src/main/java/com/ie303m22/laptopweb/services/ImageStorageService.java
@@ -1,5 +1,115 @@
-// package com.ie303m22.laptopweb.services;
+package com.ie303m22.laptopweb.services;
 
-// public class ImageStorageService implements IStogageService{
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@Service
+public class ImageStorageService implements IStorageService{
+
+    private final Path storageFolder = Paths.get("uploads");
+    //constructor 
+    public ImageStorageService() {
+        try {
+            Files.createDirectories(storageFolder);
+        }catch (IOException exception) {
+            throw new RuntimeException("Cannot initialize storage", exception);
+        }
+    }
+    private boolean isImageFile(MultipartFile file) {
+        //Let install FileNameUtils
+        String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+        return Arrays.asList(new String[] {"png","jpg","jpeg", "bmp"})
+                .contains(fileExtension.trim().toLowerCase());
+    }
+    @Override
+    public String storeFile(MultipartFile file) {
+        try{
+            if(file.isEmpty()){
+                throw new RuntimeException("Không thể lưu trữ tệp trống.");
+            }
+            //kiem tra file anh
+            if(!isImageFile(file)){
+                throw new RuntimeException("Bạn chỉ có thể tải tệp hình anhe.");
+            }
+            //file <= 5Mb
+            float fileSizeInMegabyte = file.getSize() / 1_000_000;
+            if(fileSizeInMegabyte > 5.0f){
+                throw new RuntimeException("File phải <= 5Mb.");
+            }
+            //file must be rename
+            String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+            String generatedFileName = UUID.randomUUID().toString().replace("-", "");
+            generatedFileName = generatedFileName+"."+fileExtension;
+            Path destinationFilePath = this.storageFolder.resolve(
+                Paths.get(generatedFileName))
+                .normalize().toAbsolutePath();
+            if(!destinationFilePath.getParent().equals(this.storageFolder.toAbsolutePath())){
+                throw new RuntimeException("Không thể lưu trữ tệp bên ngoài thư mục hiện tại.");
+            }
+            try(InputStream inputStream = file.getInputStream()){
+                Files.copy(inputStream, destinationFilePath, StandardCopyOption.REPLACE_EXISTING);
+            }
+            return generatedFileName;
+
+        }
+        catch(IOException exception){
+            throw new RuntimeException("Không lưu trữ được tệp. ", exception);
+        }
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            //list all files in storageFolder
+            //How to fix this ?
+            return Files.walk(this.storageFolder, 1)
+                    .filter(path -> !path.equals(this.storageFolder) && !path.toString().contains("._"))
+                    .map(this.storageFolder::relativize);
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Failed to load stored files", e);
+        }
+    }
+
+    @Override
+    public byte[] readFileContent(String fileName) {
+        try {
+            Path file = storageFolder.resolve(fileName);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                byte[] bytes = StreamUtils.copyToByteArray(resource.getInputStream());
+                return bytes;
+            }
+            else {
+                throw new RuntimeException(
+                        "Could not read file: " + fileName);
+            }
+        }
+        catch (IOException exception) {
+            throw new RuntimeException("Could not read file: " + fileName, exception);
+        }
+    }
+
+    @Override
+    public void deleteAllFile() {
+        // TODO Auto-generated method stub
+        
+    }
+
     
-// }
+    
+}
