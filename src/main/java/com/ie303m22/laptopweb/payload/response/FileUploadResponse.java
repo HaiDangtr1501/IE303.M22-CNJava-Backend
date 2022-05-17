fe45604a7bc71f58efa02ac95eb41de93f659ba9
HaiDangtr1501 <haidangtr1501@gmail.com>
2022-05-17 19:56:50 +0700

    .

---
+++ src/main/java/com/ie303m22/laptopweb/payload/response/FileUploadResponse.java
@@ -0,0 +1,27 @@
+package com.ie303m22.laptopweb.payload.response;
+
+public class FileUploadResponse {
+    private String name;
+	private String url;
+
+	public FileUploadResponse(String name, String url) {
+		this.name = name;
+		this.url = url;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
