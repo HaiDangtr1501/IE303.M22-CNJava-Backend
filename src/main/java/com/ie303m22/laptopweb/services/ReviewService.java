fe9ee48ed00beb64415304055ceca031017ae6eb
quangkhai76d1 <18520876@gm.uit.edu.vn>
2022-06-12 09:55:40 +0700

    Review

---
+++ src/main/java/com/ie303m22/laptopweb/services/ReviewService.java
@@ -0,0 +1,16 @@
+package com.ie303m22.laptopweb.services;
+
+import java.util.List;
+
+import com.congnghejava.webbanhang.models.Product;
+import com.congnghejava.webbanhang.models.Review;
+import com.congnghejava.webbanhang.models.User;
+
+public interface ReviewService extends IGeneralService<Review> {
+
+	public List<Review> findByProduct(Product product);
+
+	public boolean existsByUserAndProduct(User user, Product product);
+
+	public Review findByUser(User user);
+}
---
+++ src/main/java/com/ie303m22/laptopweb/services/ReviewServiceImpl.java
@@ -0,0 +1,55 @@
+package com.ie303m22.laptopweb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.congnghejava.webbanhang.models.Product;
+import com.congnghejava.webbanhang.models.Review;
+import com.congnghejava.webbanhang.models.User;
+import com.congnghejava.webbanhang.repository.ReviewRepository;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+	@Autowired
+	ReviewRepository reviewRepository;
+
+	@Override
+	public List<Review> findAll() {
+		return reviewRepository.findAll();
+	}
+
+	@Override
+	public Optional<Review> findById(Long theId) {
+		return reviewRepository.findById(theId);
+	}
+
+	@Override
+	public Review save(Review theReview) {
+		return reviewRepository.save(theReview);
+	}
+
+	@Override
+	public void remove(Long theId) {
+		reviewRepository.deleteById(theId);
+	}
+
+	@Override
+	public List<Review> findByProduct(Product product) {
+		return reviewRepository.findByProduct(product);
+	}
+
+	@Override
+	public boolean existsByUserAndProduct(User user, Product product) {
+		return reviewRepository.existsByUserAndProduct(user, product);
+	}
+
+	@Override
+	public Review findByUser(User user) {
+		return reviewRepository.findByUser(user);
+	}
+
+}
